23ce6fe054b90ca810ccea42adb6093d7ec2d107
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-15 14:05:04 -0600

    Implementation of fifth user story, deleting student

--- spec/features/user_can_delete_a_student_spec.rb
+++ spec/features/user_can_delete_a_student_spec.rb
@@ -4,10 +4,14 @@ describe 'User' do
   it 'can delete a student and be shown the index without the student' do
     student_1 = Student.create(name: 'Bobby')
     student_2 = Student.create(name: 'Xavier')
-    
+
     visit(students_path)
-    save_and_open_page
-    click_on('Delete')
+
+    expect(page).to have_selector(:link, 'Delete')
+
+    within('.student-list') do
+      find(:css, '.individual-student', text: student_1.name).click_on('Delete')
+    end
 
     expect(current_path).to eq(students_path)
     expect(page).to_not have_content(student_1.name)
