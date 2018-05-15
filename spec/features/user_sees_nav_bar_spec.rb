f55b5c199217a429c6a0d8d747b370efea5cccea
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-15 14:18:17 -0600

    More tests for navigation

--- spec/features/user_sees_nav_bar_spec.rb
+++ spec/features/user_sees_nav_bar_spec.rb
@@ -7,4 +7,22 @@ describe 'User' do
     expect(page).to have_link('Index', href: students_path)
     expect(page).to have_link('Create a New Student', href: new_student_path)
   end
+
+  it 'takes the user to the index if the user clicks on Index' do
+    visit(new_student_path)
+
+    click_on('Index')
+
+    expect(current_path).to eq(students_path)
+    expect(current_path).to_not eq(new_student_path)
+  end
+
+  it 'takes the user to the new student creation form if the user clicks on Create a New Student' do
+    visit(students_path)
+
+    click_on('Create a New Student')
+
+    expect(current_path).to eq(new_student_path)
+    expect(current_path).to_not eq(students_path)
+  end
 end
